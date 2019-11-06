3d8cd87a971dd6041e6973274f9e6f40cea1dfa4
icyfox-bupt <icyfox330@gmail.com>
2019-11-06 19:04:10 +0800

    Add RBAC support for this plugin.

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -3,12 +3,19 @@
 # Inbuilt Imports
 from flask import Blueprint
 from airflow.plugins_manager import AirflowPlugin
+from airflow import configuration
 
 # Backfill Plugin Imports
 from backfill.main import Backfill
 
+# Get RBAC config.
+rbac_authentication_enabled = configuration.getboolean("webserver", "RBAC")
+
 # Init the plugin in Webserver's "Admin" Menu with Menu Item as "Backfill"
-backfill_admin_view = Backfill(category="Admin", name="Backfill")
+if rbac_authentication_enabled == True:
+    backfill_admin_view = {"category" : "Admin", "name" : "Backfill",  "view": Backfill()}
+else:
+    backfill_admin_view = Backfill(category="Admin", name="Backfill")
 
 # Creating a flask blueprint to integrate the templates folder
 backfill_blueprint = Blueprint(
@@ -20,3 +27,4 @@ class AirflowBackfillPlugin(AirflowPlugin):
     name = "backfill_plugin"
     admin_views = [backfill_admin_view]
     flask_blueprints = [backfill_blueprint]
+    appbuilder_views = [backfill_admin_view]
--- plugins/backfill/main.py
+++ plugins/backfill/main.py
@@ -10,20 +10,25 @@ import datetime
 import flask
 from flask import request
 from flask_admin import BaseView, expose
+from flask_appbuilder import expose as app_builder_expose, BaseView as AppBuilderBaseView,has_access
+from airflow import configuration
 
 from shelljob import proc
 
 # Inspired from
 # https://mortoray.com/2014/03/04/http-streaming-of-command-output-in-python-flask/
 # https://www.endpoint.com/blog/2015/01/28/getting-realtime-output-using-python
+# RBAC inspired from 
+# https://github.com/teamclairvoyant/airflow-rest-api-plugin
 
 
 # Set your Airflow home path
-airflow_home_path = '/home/airflow/airflow'
+airflow_home_path = os.environ['AIRFLOW_HOME']
 
 # Local file where history will be stored
 FILE = airflow_home_path + '/logs/backfill_history.txt'
 
+rbac_authentication_enabled = configuration.getboolean("webserver", "RBAC")
 
 # Creating a flask admin BaseView
 def file_ops(mode, data=None):
@@ -44,15 +49,30 @@ def file_ops(mode, data=None):
             f.write(file_data)
             return 1
 
+def get_baseview():
+    if rbac_authentication_enabled == True:
+        return AppBuilderBaseView
+    else:
+        return BaseView
 
-class Backfill(BaseView):
+class Backfill(get_baseview()):
 
-    @expose('/')
-    def base(self):
-        """ Render the backfill page to client """
-        return self.render("backfill_page.html")
+    route_base = "/admin/backfill/"
+
+    if rbac_authentication_enabled == True:
+        @app_builder_expose('/')
+        def list(self):
+            """ Render the backfill page to client with RBAC"""
+            return self.render_template("backfill_page.html",
+                                        rbac_authentication_enabled=rbac_authentication_enabled)        
+    else:
+        @expose('/')
+        def base(self):
+            """ Render the backfill page to client """
+            return self.render("backfill_page.html")
 
     @expose('/stream')
+    @app_builder_expose('/stream')
     def stream(self):
         """ Runs user request and outputs console stream to client"""
         dag_name = request.args.get("dag_name")
@@ -84,6 +104,7 @@ class Backfill(BaseView):
         return flask.Response(read_process(), mimetype='text/event-stream')
 
     @expose('/background')
+    @app_builder_expose('/background')
     def background(self):
         """ Runs user request in background """
         dag_name = request.args.get("dag_name")
@@ -111,6 +132,7 @@ class Backfill(BaseView):
         return flask.Response(response, mimetype='text/json')
 
     @expose('/history')
+    @app_builder_expose('/history')
     def history(self):
         """ Outputs recent user request history """
         return flask.Response(file_ops('r'), mimetype='text/txt')
